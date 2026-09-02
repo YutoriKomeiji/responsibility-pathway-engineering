129d1c107e02e9c000948a9b24ac555ff70f8225
Akihisa Ono / 小野 昭久（ PN: Yutori Komeiji / 古明地ゆとり ）ID: dantarg <akihisa.ono.0118@gmail.com>
2026-09-02 15:09:45 +0900

    m3: export Human Return readiness evaluator

--- rpe_kernel/__init__.py
+++ rpe_kernel/__init__.py
@@ -1,6 +1,7 @@
 """Public Python API for the RPE external responsibility kernel."""
 
 from .gateway import evaluate_gateway_request, evaluate_integrity_guarded_gateway_request, evaluate_transition
+from .human_return import evaluate_human_return_readiness, human_return_result_to_risk_condition
 from .integrity import compare_integrity_binding, integrity_result_to_risk_condition
 from .loader import LoaderError, load_governed_envelope_content, load_governed_envelope_file
 from .pipeline import evaluate_action, evaluate_governed_action
@@ -12,9 +13,11 @@ __all__ = [
     "evaluate_action",
     "evaluate_gateway_request",
     "evaluate_governed_action",
+    "evaluate_human_return_readiness",
     "evaluate_integrity_guarded_gateway_request",
     "evaluate_risk_conditions",
     "evaluate_transition",
+    "human_return_result_to_risk_condition",
     "integrity_result_to_risk_condition",
     "load_governed_envelope_content",
     "load_governed_envelope_file",
