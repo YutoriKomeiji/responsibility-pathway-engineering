c8687cc6f0bf988ef915443a2806f6987b40386e
Akihisa Ono / 小野 昭久（ PN: Yutori Komeiji / 古明地ゆとり ）ID: dantarg <akihisa.ono.0118@gmail.com>
2026-09-02 13:36:13 +0900

    m3: expose thin gateway request API

--- rpe_kernel/__init__.py
+++ rpe_kernel/__init__.py
@@ -1,6 +1,6 @@
 """Public Python API for the RPE external responsibility kernel."""
 
-from .gateway import evaluate_transition
+from .gateway import evaluate_gateway_request, evaluate_transition
 from .loader import LoaderError, load_governed_envelope_content, load_governed_envelope_file
 from .pipeline import evaluate_action, evaluate_governed_action
 from .risk_conditions import evaluate_risk_conditions
@@ -8,6 +8,7 @@ from .risk_conditions import evaluate_risk_conditions
 __all__ = [
     "LoaderError",
     "evaluate_action",
+    "evaluate_gateway_request",
     "evaluate_governed_action",
     "evaluate_risk_conditions",
     "evaluate_transition",
