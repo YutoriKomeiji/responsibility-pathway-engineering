35fa9d87ffd1a5778db648329728e094a0f5c363
Akihisa Ono / 小野 昭久（ PN: Yutori Komeiji / 古明地ゆとり ）ID: dantarg <akihisa.ono.0118@gmail.com>
2026-09-02 15:03:20 +0900

    m3: export integrity-guarded Python gateway

--- rpe_kernel/__init__.py
+++ rpe_kernel/__init__.py
@@ -1,6 +1,6 @@
 """Public Python API for the RPE external responsibility kernel."""
 
-from .gateway import evaluate_gateway_request, evaluate_transition
+from .gateway import evaluate_gateway_request, evaluate_integrity_guarded_gateway_request, evaluate_transition
 from .integrity import compare_integrity_binding, integrity_result_to_risk_condition
 from .loader import LoaderError, load_governed_envelope_content, load_governed_envelope_file
 from .pipeline import evaluate_action, evaluate_governed_action
@@ -12,6 +12,7 @@ __all__ = [
     "evaluate_action",
     "evaluate_gateway_request",
     "evaluate_governed_action",
+    "evaluate_integrity_guarded_gateway_request",
     "evaluate_risk_conditions",
     "evaluate_transition",
     "integrity_result_to_risk_condition",
